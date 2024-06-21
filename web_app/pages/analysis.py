b4af549648bdae454416ac63dcae2fa4d0eb588a
Yatharth Verma <b22cs064@iitj.ac.in>
2024-06-21 20:58:17 +0530

    made changes

--- web_app/pages/analysis.py
+++ web_app/pages/analysis.py
@@ -10,8 +10,8 @@ st.set_page_config(page_title="Plotting Demo")
 
 st.title('Analytics')
 
-new_df = pd.read_csv('/Users/Yatharth/Desktop/Real-Estate/Real-Estate-property-Prediction/web_app/datasets/data_viz1.csv')
-feature_text = pickle.load(open('/Users/Yatharth/Desktop/Real-Estate/Real-Estate-property-Prediction/web_app/datasets/feature_text.pkl','rb'))
+new_df = pd.read_csv('/web_app/datasets/data_viz1.csv')
+feature_text = pickle.load(open('/web_app/datasets/feature_text.pkl','rb'))
 
 group_df = new_df.groupby('sector')[['price','price_per_sqft','built_up_area','latitude','longitude']].mean()
 
@@ -21,23 +21,6 @@ fig = px.scatter_mapbox(group_df, lat="latitude", lon="longitude", color="price_
                   mapbox_style="open-street-map",width=1200,height=700,hover_name=group_df.index)
 
 st.plotly_chart(fig,use_container_width=True)
-
-st.header('Features Wordcloud')
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-wordcloud = WordCloud(width = 800, height = 800,
-                      background_color ='black',
-                      stopwords = set(['s']),  # Any stopwords you'd like to exclude
-                      min_font_size = 10).generate(feature_text)
-
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.tight_layout(pad = 0)
-st.pyplot()
-
-
 
 st.header('Area Vs Price')
 
